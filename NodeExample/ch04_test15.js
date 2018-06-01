33cd2e4730aaef5b2ad88a2db325f9ae924e5d13
heocules <heogw0220@naver.com>
2018-06-02 04:15:18 +0900

    0602

--- NodeExample/ch04_test15.js
+++ NodeExample/ch04_test15.js
@@ -1,11 +1,10 @@
-var winston = require('winston');    				// 로그 처리 모듈
+var winston = require('winston'); 				// 로그 처리 모듈
 var winstonDaily = require('winston-daily-rotate-file');    	// 로그 일별 처리 모듈
 var moment = require('moment');    				// 시간 처리 모듈
 
 function timeStampFormat() {
-    return moment().format('YYYY-MM-DD HH:mm:ss.SSS ZZ'); // '2016-05-01 20:14:28.500 +0900'
-};
-
+    return moment().format('YYYY-MM-DD HH:mm:ss.SSS ZZ');
+}
 
 var logger = new (winston.Logger)({
     transports: [
